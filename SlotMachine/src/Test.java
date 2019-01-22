5c8ee1523a4e6d3f3b04717981349f48996610a3
mgrddsj <Jesse_Xu@live.com>
2019-01-23 01:48:58 +0800

    Got Enter key working. Added a background image.

--- SlotMachine/src/Test.java
+++ SlotMachine/src/Test.java
@@ -12,8 +12,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
-public class Test implements ActionListener, KeyListener{
+import javax.swing.JButton;
+
+public class Test{
 
 	private JFrame frmSlotMachine;
 
@@ -50,64 +53,98 @@ public class Test implements ActionListener, KeyListener{
 		frmSlotMachine.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmSlotMachine.getContentPane().setLayout(null);
 		
-		JLabel label = new JLabel("");
+		//Setup images that needed to be used. 
+		Image background = new ImageIcon(this.getClass().getResource("/NewSlotMachineBackground.jpg")).getImage();
+		Image bio = new ImageIcon(this.getClass().getResource("/biology.png")).getImage();
+		Image chem = new ImageIcon(this.getClass().getResource("/chemistry.png")).getImage();
+		Image chinese = new ImageIcon(this.getClass().getResource("/chinese.png")).getImage();
 		Image cs = new ImageIcon(this.getClass().getResource("/cs.png")).getImage();
+		Image eng = new ImageIcon(this.getClass().getResource("/english.png")).getImage();
+		Image his = new ImageIcon(this.getClass().getResource("/history.png")).getImage();
+		Image math = new ImageIcon(this.getClass().getResource("/maths.png")).getImage();
+		Image physics = new ImageIcon(this.getClass().getResource("/physics.png")).getImage();
+		
+		String[] picNames = new String[8];//The equivalent number of the picture. 
+		picNames[0] = "bio";
+		picNames[1] = "chem";
+		picNames[2] = "chinese";
+		picNames[3] = "cs";
+		picNames[4] = "eng";
+		picNames[5] = "his";
+		picNames[6] = "math";
+		picNames[7] = "physics";
+		
+		JLabel label = new JLabel("");
 		label.setIcon(new ImageIcon(cs));
-		label.setBounds(197, 250, 128, 128);
+		label.setBounds(205, 275, 128, 128);
 		frmSlotMachine.getContentPane().add(label);
 		
 		JLabel label_1 = new JLabel("");
-		Image eng = new ImageIcon(this.getClass().getResource("/english.png")).getImage();
 		label_1.setIcon(new ImageIcon(eng));
-		label_1.setBounds(197, 400, 128, 128);
+		label_1.setBounds(205, 425, 128, 128);
 		frmSlotMachine.getContentPane().add(label_1);
 		
 		JLabel label_2 = new JLabel("");
-		Image his = new ImageIcon(this.getClass().getResource("/history.png")).getImage();
 		label_2.setIcon(new ImageIcon(his));
-		label_2.setBounds(197, 100, 128, 128);
+		label_2.setBounds(205, 125, 128, 128);
 		frmSlotMachine.getContentPane().add(label_2);
 		
-		JLabel button = new JLabel("");
+		JLabel label_3 = new JLabel("");
+		label_3.setIcon(new ImageIcon(bio));
+		label_3.setBounds(576, 125, 128, 128);
+		frmSlotMachine.getContentPane().add(label_3);
+		
+		JLabel label_4 = new JLabel("");
+		label_4.setIcon(new ImageIcon(chem));
+		label_4.setBounds(576, 275, 128, 128);
+		frmSlotMachine.getContentPane().add(label_4);
+		
+		JLabel label_5 = new JLabel("");
+		label_5.setIcon(new ImageIcon(chinese));
+		label_5.setBounds(576, 425, 128, 128);
+		frmSlotMachine.getContentPane().add(label_5);
+		
+		JLabel label_6 = new JLabel("");
+		label_6.setIcon(new ImageIcon(physics));
+		label_6.setBounds(949, 125, 128, 128);
+		frmSlotMachine.getContentPane().add(label_6);
+		
+		JLabel label_7 = new JLabel("");
+		label_7.setIcon(new ImageIcon(math));
+		label_7.setBounds(949, 275, 128, 128);
+		frmSlotMachine.getContentPane().add(label_7);
+		
+		JLabel label_8 = new JLabel("");
+		label_8.setIcon(new ImageIcon(physics));
+		label_8.setBounds(949, 425, 128, 128);
+		frmSlotMachine.getContentPane().add(label_8);
+		
+		JButton button = new JButton("");
+		button.addKeyListener(new KeyAdapter()
+		{
+			@Override
+			public void keyPressed(KeyEvent e)
+			{
+				if (e.getKeyCode() == KeyEvent.VK_ENTER)
+				{
+					label.setVisible(false);
+					System.out.println("Key pressed! ");
+				}
+			}
+		});
 		Image bt = new ImageIcon(this.getClass().getResource("/button.gif")).getImage();
-		button.setIcon(new ImageIcon(bt)); 
+		button.setIcon(new ImageIcon(bt));
 		button.setBounds(491, 514, 245, 186);
 		frmSlotMachine.getContentPane().add(button);
 		
+				//Random rand = new Random();
+				
+				//Background img
+				JLabel Background = new JLabel("");
+				Background.setIcon(new ImageIcon(background));
+				Background.setBounds(0, 0, 1262, 681);
+				frmSlotMachine.getContentPane().add(Background);
 		
-	}
 
-	@Override
-	public void keyTyped(KeyEvent e) 
-	{
-		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
-	public void keyPressed(KeyEvent e) 
-	{
-		if (e.getKeyCode()==KeyEvent.VK_ENTER)
-		{
-			System.out.println("Hello");
-		}
-		Component frame = new JFrame();
-		JOptionPane.showMessageDialog(frame , "You just hit the enter. ");		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) 
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) 
-	{
-		// TODO Auto-generated method stub
-		
-	}
-	
-
 }
