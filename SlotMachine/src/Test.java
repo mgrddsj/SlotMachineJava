7166ecc313b1a9bc42e7506685e874a65daf54bd
mgrddsj <Jesse_Xu@live.com>
2019-01-22 08:51:16 +0800

    Today's work

--- SlotMachine/src/Test.java
+++ SlotMachine/src/Test.java
@@ -1,8 +1,11 @@
 import java.awt.EventQueue;
 import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Test {
 
@@ -42,8 +45,40 @@ public class Test {
 		frmSlotMachine.getContentPane().setLayout(null);
 		
 		JLabel label = new JLabel("");
-		Image cs = new ImageIcon(this.getClass().getResource("/cs.png").getImage());
-		label.setBounds(197, 259, 128, 128);
+		Image cs = new ImageIcon(this.getClass().getResource("/cs.png")).getImage();
+		label.setIcon(new ImageIcon(cs));
+		label.setBounds(197, 250, 128, 128);
 		frmSlotMachine.getContentPane().add(label);
+		
+		JLabel label_1 = new JLabel("");
+		Image eng = new ImageIcon(this.getClass().getResource("/english.png")).getImage();
+		label_1.setIcon(new ImageIcon(eng));
+		label_1.setBounds(197, 400, 128, 128);
+		frmSlotMachine.getContentPane().add(label_1);
+		
+		JLabel label_2 = new JLabel("");
+		Image his = new ImageIcon(this.getClass().getResource("/history.png")).getImage();
+		label_2.setIcon(new ImageIcon(his));
+		label_2.setBounds(197, 100, 128, 128);
+		frmSlotMachine.getContentPane().add(label_2);
+		
+		JLabel button = new JLabel("");
+		button.addKeyListener(new KeyAdapter() 
+		{
+			@Override
+			public void keyPressed(KeyEvent e) 
+			{
+				if (e.getKeyCode()==KeyEvent.VK_ENTER)
+				{
+		            label.setVisible(false);
+		        }
+				
+			}
+		});
+		
+		Image bt = new ImageIcon(this.getClass().getResource("/button.gif")).getImage();
+		button.setIcon(new ImageIcon(bt)); 
+		button.setBounds(491, 514, 245, 186);
+		frmSlotMachine.getContentPane().add(button);
 	}
 }
